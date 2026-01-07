b99f7bf4c8a8610c18dc9b9b8620e21a919dbd88
ci-robot <ci-robot@k8s.io>
2026-01-07 18:42:37 +0000

    Update to ACK runtime `v0.57.0`, code-generator `v0.57.0`

--- pkg/resource/state_machine/delta.go
+++ pkg/resource/state_machine/delta.go
@@ -17,16 +17,15 @@ package state_machine
 
 import (
 	"bytes"
-	"reflect"
 
 	ackcompare "github.com/aws-controllers-k8s/runtime/pkg/compare"
 	acktags "github.com/aws-controllers-k8s/runtime/pkg/tags"
+	"k8s.io/apimachinery/pkg/api/equality"
 )
 
 // Hack to avoid import errors during build...
 var (
 	_ = &bytes.Buffer{}
-	_ = &reflect.Method{}
 	_ = &acktags.Tags{}
 )
 
@@ -57,7 +56,7 @@ func newResourceDelta(
 		if len(a.ko.Spec.LoggingConfiguration.Destinations) != len(b.ko.Spec.LoggingConfiguration.Destinations) {
 			delta.Add("Spec.LoggingConfiguration.Destinations", a.ko.Spec.LoggingConfiguration.Destinations, b.ko.Spec.LoggingConfiguration.Destinations)
 		} else if len(a.ko.Spec.LoggingConfiguration.Destinations) > 0 {
-			if !reflect.DeepEqual(a.ko.Spec.LoggingConfiguration.Destinations, b.ko.Spec.LoggingConfiguration.Destinations) {
+			if !equality.Semantic.Equalities.DeepEqual(a.ko.Spec.LoggingConfiguration.Destinations, b.ko.Spec.LoggingConfiguration.Destinations) {
 				delta.Add("Spec.LoggingConfiguration.Destinations", a.ko.Spec.LoggingConfiguration.Destinations, b.ko.Spec.LoggingConfiguration.Destinations)
 			}
 		}
